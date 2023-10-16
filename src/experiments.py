da1d0ba1a9defe8a00189ca53d0c309b81fb6490
HCA97 <h.cemaltun1997@gmail.com>
2023-10-16 23:33:17 +0200

    some more experiments

--- src/experiments.py
+++ src/experiments.py
@@ -179,11 +179,15 @@ class ExperimentMosquitoClassifier:
         freeze_backbones: bool = False,
         warm_up_steps: int = 2000,
         epochs: int = 5,
-        n_splits: int = 5,
+        label_smoothing: float = 0.0,
+        img_size: Tuple[int, int] = (224, 224),
         create_callbacks: Callable[[], List[Callback]] = _default_callbacks,
+        use_same_split_as_yolo: bool = False,
+        use_ema: bool = False,
+        shift_box: bool = False,
     ):
         annotations_df = pd.read_csv(self.annotations_csv)
-        skf = StratifiedKFold(n_splits=n_splits, random_state=200)
+        skf = StratifiedKFold(n_splits=5, random_state=200)
 
         for _, (train_index, val_index) in enumerate(
             skf.split(annotations_df, annotations_df.class_label)
@@ -192,7 +196,7 @@ class ExperimentMosquitoClassifier:
             val_df = annotations_df.iloc[val_index]
 
             train_dataloader, val_dataloader = self.get_dataloaders(
-                train_df, val_df, model_name, data_aug, bs
+                train_df, val_df, dataset, data_aug, bs, img_size, shift_box
             )
 
             th.set_float32_matmul_precision("high")
@@ -206,6 +210,11 @@ class ExperimentMosquitoClassifier:
                 bs=bs,
                 data_aug=data_aug,
                 epochs=epochs,
+                label_smoothing=label_smoothing,
+                img_size=img_size,
+                use_ema=use_ema,
+                use_same_split_as_yolo=use_same_split_as_yolo,
+                shift_box=shift_box,
             )
             trainer = pl.Trainer(
                 accelerator="gpu",
