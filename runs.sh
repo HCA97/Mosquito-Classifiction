4c04e2d4430da3713099cf4d6e4f9824fc4071e6
HCA97 <h.cemaltun1997@gmail.com>
2023-10-20 20:23:11 +0200

    bla

--- runs.sh
+++ runs.sh
@@ -1,5 +1,15 @@
 
-python mosquito_clf_final.py
+# python mosquito_clf_yolo_lux.py
+
+# python mosquito_clf_yolo_lux_ema.py
+
+# python mosquito_clf_mosq_lux.py
+
+# python mosquito_clf_mosq_lux_ema.py
+
+# python mosquito_clf_competition_yolo_inat_only_rare.py
+
+# python mosquito_clf_competition_yolo_inat_only_rare_ema.py
 
 cd yolo
 
